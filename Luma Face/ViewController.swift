3d32d3b66d817087c7913f8ae8bbfd37a3080d0e
Anton Heestand <anton.heestand@gmail.com>
2018-11-24 02:14:10 +0100

    Initial Commit

---
+++ Luma Face/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Luma Face
+//
+//  Created by Hexagons on 2018-11-24.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
